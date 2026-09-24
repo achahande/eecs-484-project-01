9d392c70e2ba312ff3a5dfc0c8e560672952acfd
A Chahande <105685613+achahande@users.noreply.github.com>
2026-09-24 12:30:24 -0400

    Create initial files

---
+++ dropTables.sql
@@ -0,0 +1,15 @@
+-- DROP TABLES
+DROP TABLE Tags;
+DROP TABLE Photos;
+DROP TABLE Albums;
+DROP TABLE Participants;
+DROP TABLE User_Events;
+DROP TABLE Education;
+DROP TABLE Programs;
+DROP TABLE Messages;
+DROP TABLE User_Hometown_Cities;
+DROP TABLE User_Current_Cities;
+DROP TABLE Friends;
+DROP TABLE Cities;
+DROP TABLE Users;
+DROP SEQUENCE Program_ID_Seq;
