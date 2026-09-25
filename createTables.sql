a349eab201c82f16a1a21d30941ddf5f73808c38
A Chahande <105685613+achahande@users.noreply.github.com>
2026-09-24 21:10:35 -0400

    Fixed issues with circular dependency

--- createTables.sql
+++ createTables.sql
@@ -135,13 +135,17 @@ CREATE TABLE Photos (
     photo_created_time TIMESTAMP NOT NULL,
     photo_modified_time TIMESTAMP,
     photo_link VARCHAR2(2000) NOT NULL,
-    PRIMARY KEY (photo_id),
-    FOREIGN KEY (album_id) REFERENCES Albums(album_id)
+    PRIMARY KEY (photo_id)
 );
 
 ALTER TABLE Albums
 ADD FOREIGN KEY (cover_photo_id)
 REFERENCES Photos(photo_id)
+INITIALLY DEFERRED DEFERRABLE;
+
+ALTER TABLE Photos
+ADD FOREIGN KEY (album_id)
+REFERENCES Albums(album_id)
 INITIALLY DEFERRED DEFERRABLE;
 
 CREATE TABLE Tags (
